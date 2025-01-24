1da18b2a90d46e9ed38d69cd3a06d19f2abc103a
Nami Sunami <nsunami@pm.me>
2025-01-24 19:13:49 +0100

    feat(get_s1c_sensitivity_APA): Refactor code to get rejection sensitivity for Study 1 data

--- r/get_s1c_sensitivity_APA.R
+++ r/get_s1c_sensitivity_APA.R
@@ -1,15 +1,24 @@
 get_s1c_sensitivity_APA <- function(df) {
-    df %>%
-        describe_by_factor(acceptance, vars = heart2) %>%
-        mutate(
-            msd = to_msd(heart2_mean, heart2_sd),
-            acceptance = to_factor(acceptance)
-        ) %>%
-        select(acceptance, msd) %>%
-        deframe() %>%
-        as.list() %>%
-        append(list(
-            t = s1c_sensitivity_mod %>% describe.ttest(),
-            d = describe_d_ci(s1c_sensitivity_d)
-        ))
+  s1c_sensitivity_mod <- t.test(formula = heart2 ~ acceptance, data = df)
+
+  ## Get Cohen's d
+  s1c_sensitivity_d <- effectsize::cohens_d(
+    heart2 ~ acceptance,
+    data = df,
+    ci = .95
+  )
+
+  df |>
+    describe_by_factor(acceptance, vars = heart2) |>
+    mutate(
+      msd = to_msd(heart2_mean, heart2_sd),
+      acceptance = to_factor(acceptance)
+    ) |>
+    select(acceptance, msd) |>
+    deframe() |>
+    as.list() |>
+    append(list(
+      t = s1c_sensitivity_mod |> describe.ttest(),
+      d = describe_d_ci(s1c_sensitivity_d)
+    ))
 }
