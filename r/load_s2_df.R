41c0e327ab6f8439f819508404bc453091138385
Nami Sunami <nsunami@pm.me>
2025-01-25 00:04:45 +0100

    fix(load_s2_df): State packages for functions

--- r/load_s2_df.R
+++ r/load_s2_df.R
@@ -1,9 +1,14 @@
 # Load Study 2 Data
 load_s2_df <- function() {
-    s2_df <- read_rds(here("data_public", "Study2_public.rds"))
-    ## Factorize Essay Conditions
-    s2_df %>%
-        mutate(essay_condition = factor(essay_condition,
-            levels = c("Social Surrogacy", "Non-Social Surrogacy")
-        ))
+    s2_df <- readRDS(
+        here::here("data_public", "Study2_public.rds")
+    )
+    ## Factorize Essay Conditionsw
+    s2_df |>
+        dplyr::mutate(
+            essay_condition = factor(
+                essay_condition,
+                levels = c("Social Surrogacy", "Non-Social Surrogacy")
+            )
+        )
 }
