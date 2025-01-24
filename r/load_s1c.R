28855329eeb2bc78e99f71dbe144b4e3943800aa
Nami Sunami <nsunami@pm.me>
2025-01-25 00:24:18 +0100

    fix(load_s1c): Fix the issue of conditionals not working

--- r/load_s1c.R
+++ r/load_s1c.R
@@ -1,24 +1,29 @@
 load_s1c <- function() {
   df <- readRDS(here::here("data_public/Study1c_public.rds"))
 
-  df |>
+  df <- df |>
     dplyr::mutate(grouping_dummy = dplyr::case_when(
-      labels(acceptance) == "0" & labels(closeness) == "0" ~ "Rejected by Stranger",
-      labels(acceptance) == "1" & labels(closeness) == "0" ~ "Accepted by Stranger",
-      labels(acceptance) == "0" & labels(closeness) == "1" ~ "Rejected by Close Other",
-      labels(acceptance) == "1" & labels(closeness) == "1" ~ "Accepted by Close Other",
+      as.character(acceptance) == "0" & as.character(closeness) == "0" ~ "Rejected by Stranger",
+      as.character(acceptance) == "1" & as.character(closeness) == "0" ~ "Accepted by Stranger",
+      as.character(acceptance) == "0" & as.character(closeness) == "1" ~ "Rejected by Close Other",
+      as.character(acceptance) == "1" & as.character(closeness) == "1" ~ "Accepted by Close Other",
       TRUE ~ NA_character_
     )) |>
     # Factors should be effect-coded
     # Effect coding the factors
     dplyr::mutate(
       acceptanceEC = dplyr::case_when(
-        labels(acceptance) == "0" ~ -0.5,
-        labels(acceptance) == "1" ~ 0.5
+        as.character(acceptance) == "0" ~ -0.5,
+        as.character(acceptance) == "1" ~ 0.5
       ),
       closenessEC = dplyr::case_when(
-        labels(closeness) == "0" ~ -0.5,
-        labels(closeness) == "1" ~ 0.5
+        as.character(closeness) == "0" ~ -0.5,
+        as.character(closeness) == "1" ~ 0.5
       )
     )
+
+  # Check if the resulting data frame includes expected values
+  if (!("Rejected by Stranger" %in% df$grouping_dummy)) {
+    stop("Rejected by Stranger was not found in the dummies")
+  }
 }
